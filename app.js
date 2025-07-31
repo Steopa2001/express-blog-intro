0764e20f52965532f2e28b9a61158dba22de8bd7
Steopa2001 <moraru495@gmail.com>
2025-07-31 14:53:23 +0200

    define a port on which the server will listen

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log('Hello World');
+//importo il module express
+const express = require('express');
+//creo un'istanza dell'app express
+const app = express();
+//definisco la porta su cui il server sarà in ascolto
+const port = 3000;
+//inserisco il middleware per i file statici
+app.use(express.static('public'));
+
