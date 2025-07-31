f6f2ad044cb903c3fda545d05ab525f3e75c72e9
Steopa2001 <moraru495@gmail.com>
2025-07-31 15:35:52 +0200

    end commit

--- app.js
+++ app.js
@@ -18,31 +18,31 @@ app.get('/', (req, res) => {
   {
     titolo: "Il mio primo post",
     contenuto: "Ciao a tutti! Questo è il mio primo post sul blog.",
-    immagine: "./public/img/first-post-v0-arm1qbj34wfe1.webp",
+    immagine: "/img/first-post-v0-arm1qbj34wfe1.webp",
     tags: ["introduzione", "personale"]
   },
   {
     titolo: "Una giornata al parco",
     contenuto: "Oggi ho passato una bellissima giornata al parco con gli amici.",
-    immagine: "./public/img/parco.png",
+    immagine: "/img/parco.png",
     tags: ["tempo libero", "natura"]
   },
   {
     titolo: "Ricetta facile: pasta al pomodoro",
     contenuto: "Ecco come preparare una semplice pasta al pomodoro in 10 minuti.",
-    immagine: "./public/img/pasta.avif",
+    immagine: "/img/pasta.avif",
     tags: ["cucina", "ricetta"]
   },
   {
     titolo: "I miei libri preferiti",
     contenuto: "Una lista di libri che mi hanno ispirato e che consiglio a tutti.",
-    immagine: "./public/img/libri.jpg",
+    immagine: "/img/libri.jpg",
     tags: ["libri", "consigli"]
   },
   {
     titolo: "Viaggio a Roma",
     contenuto: "Qualche foto e racconto del mio recente viaggio a Roma.",
-    immagine: "./public/img/roma.avif",
+    immagine: "/img/roma.avif",
     tags: ["viaggi", "Italia"]
   }
 ];
@@ -50,12 +50,12 @@ app.get('/', (req, res) => {
 //creo rotta /bacheca
 app.get('/bacheca', (req, res) => {
     //restituisco oggetto json
-    res.json(posts)
+    res.json({posts});
 });
 
 
 
-//avvio il server in ascolto sulla porta 3000
+//avvio il server in ascolto sulla porta 3001
 app.listen(port, () => {
     console.log(`Server attivo su http://localhost:${port}`)
 });
