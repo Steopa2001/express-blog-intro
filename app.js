129725e3a4e4f58bd80749b5bc88ce5fd0b1ad14
Steopa2001 <moraru495@gmail.com>
2025-07-31 15:03:26 +0200

    feat: start the server in the 3001 port

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ const express = require('express');
 //creo un'istanza dell'app express
 const app = express();
 //definisco la porta su cui il server sarà in ascolto
-const port = 3000;
+const port = 3001;
 //inserisco il middleware per i file statici
 app.use(express.static('public'));
 
 
 //Creo una rotta Get per ottenere un testo
+app.get('/', (req, res) => {
+    res.send('Server del mio blog');
+});
+
+//avvio il server in ascolto sulla porta 3000
+app.listen(port, () => {
+    console.log(`Server attivo su http://localhost:${port}`)
+});
